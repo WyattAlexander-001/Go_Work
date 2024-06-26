257c63a87142dd9e9235af6af30c04813e3f3cbd
WyattAlexander-001 <wyattbushman117@gmail.com>
2024-06-26 13:11:53 -0400

    I added language tests

--- learn-go-with-tests/hello/hello.go
+++ learn-go-with-tests/hello/hello.go
@@ -7,7 +7,28 @@ func Hello() string {
 	// return "Hello, world!!!!" // Will fail
 }
 
+func HelloName(name string) string {
+	return "Hello, " + name
+}
+
+func IdealHello(name string, language string) string {
+	if name == "" {
+		name = "world"
+	}
+
+	if language == "English" {
+		return "Hello, " + name
+	} else if language == "Spanish" {
+		return "Hola, " + name
+	}
+	return "Hi, " + name
+}
+
 func main() {
 	fmt.Println(Hello())
 	fmt.Println("Yay it works!")
+	fmt.Println(HelloName("Wyatt"))
+	fmt.Println(IdealHello("Wyatt", "Spanish"))
+	fmt.Println(IdealHello("", "Spanish"))
+	fmt.Println(IdealHello("", ""))
 }
--- learn-go-with-tests/hello/hello_test.go
+++ learn-go-with-tests/hello/hello_test.go
@@ -11,3 +11,33 @@ func TestHello(t *testing.T) {
 		t.Errorf("got %q want %q", got, want)
 	}
 }
+
+func TestHelloName(t *testing.T) {
+	got := HelloName("Wyatt")
+	want := "Hello, Wyatt"
+
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
+}
+
+func TestIdealHello(t *testing.T) {
+	got := IdealHello("Wyatt", "Spanish")
+	want := "Hola, Wyatt"
+
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
+
+	got = IdealHello("", "Spanish")
+	want = "Hola, world"
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
+
+	got = IdealHello("", "")
+	want = "Hi, world"
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
+}
