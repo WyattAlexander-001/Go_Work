bad6f5edd0a78db18dabf1e5a6c509c0b9809f89
WyattAlexander-001 <wyattbushman117@gmail.com>
2024-06-28 00:58:05 -0400

    working server

---
+++ webServer/src/go-server/go.mod
@@ -0,0 +1,3 @@
+module wyatt.com/m
+
+go 1.22.4
--- webServer/src/go-server/main.go
+++ webServer/src/go-server/main.go
@@ -7,7 +7,30 @@ import (
 )
 
 func helloHandler(w http.ResponseWriter, r *http.Request) { // Request = Client request Response = Server response
-	fmt.Fprintf(w, "Hello, World!")
+	if r.URL.Path != "/hello" {
+		http.Error(w, "404 not found.", http.StatusNotFound)
+		return
+	}
+
+	if r.Method != "GET" {
+		http.Error(w, "Method is not supported.", http.StatusNotFound)
+		return
+	}
+
+	fmt.Fprintf(w, "Hello!\n")
+}
+
+func formHandler(w http.ResponseWriter, r *http.Request) {
+	if err := r.ParseForm(); err != nil {
+		fmt.Fprintf(w, "ParseForm() err: %v", err)
+		return
+	}
+	fmt.Fprintf(w, "POST request successful\n")
+	name := r.FormValue("name")
+	address := r.FormValue("address")
+	fmt.Fprintf(w, "Name = %s\n", name)
+	fmt.Fprintf(w, "Address = %s\n", address)
+
 }
 
 func main() {
